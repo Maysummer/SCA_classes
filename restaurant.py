7436e2600ea6649383a143daa6137c626c390ee3
Nmesoma Udojike <42492293+Maysummer@users.noreply.github.com>
2021-04-02 12:34:58 +0100

    Add files via upload

--- restaurant.py
+++ restaurant.py
@@ -1,7 +1,25 @@
 class Restaurant:
     def __init__(self, restaurant_name, cuisine_type):
-        self.restaurant_name = name
-        self.cuisine_type = cuisine
+        self.name = restaurant_name
+        self.cuisine = cuisine_type
 
-    def describe_restaurant (self):
-        print(f"sel")
+    def describe_restaurant(self):
+        print(f"We are {self.name} and eat {self.cuisine}")
+
+    def open_restaurant(self):
+        print(f"{self.name} opens at 8:00 am daily")
+
+restaurant = Restaurant("May's food", "italian")
+print(f"This is {restaurant.name}")
+print(f"we eat {restaurant.cuisine}")
+restaurant.describe_restaurant()
+restaurant.open_restaurant()
+
+restaurant_1 = Restaurant("Kfc", "chicken")
+restaurant_1.describe_restaurant()
+
+restaurant_2 = Restaurant("Dominos", "pizza")
+restaurant_2.describe_restaurant()
+
+restaurant_3 = Restaurant("cold stone", "ice-cream")
+restaurant_3.describe_restaurant()
